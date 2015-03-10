32ced84ce0fb742e15c7b4b5ab197274f8a3cf36
snelso15 <snelso15@ycp.edu>
2015-03-10 15:33:17 -0400

    Initial commit

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject clojure-benchmarking "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [criterium "0.4.3"]
+                 ;[net.mikera/core.matrix "0.29.1"]
+                 [incanter "1.5.5"]]
+  :plugins [[lein-nodisassemble "0.1.3"]])
